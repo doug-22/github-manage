fa3630c85a233c09fda34f7faacbdeceaf7073e3
Douglas <douglas.aguiar410@gmail.com>
2023-10-01 19:01:30 -0300

    feat/created reducer to access token route

--- src/app/store/auth.ts
+++ src/app/store/auth.ts
@@ -1,11 +1,18 @@
 'use client'
 
-import { createSlice } from '@reduxjs/toolkit'
+import {
+  createSlice,
+  isFulfilled,
+  isPending,
+  isRejected,
+} from '@reduxjs/toolkit'
 import Cookie from 'js-cookie'
 import { AuthState } from '../types'
+import * as authActions from './actions/authActions'
 
 export const initialState: AuthState = {
   token: Cookie.get('TOKEN') ?? '',
+  loadingGetAccessToken: false,
 }
 
 const authSlice = createSlice({
@@ -17,12 +24,31 @@ const authSlice = createSlice({
       Cookie.remove('TOKEN')
     },
     login: (state, action) => {
-      state.token = action.payload
+      state.token = action.payload?.name
       Cookie.set('TOKEN', state.token, {
         secure: true,
         expires: 1,
       })
     },
+  },
+  extraReducers(builder) {
+    builder.addMatcher(isPending(authActions.getAccessToken), (state) => {
+      state.loadingGetAccessToken = true
+    })
+    builder.addMatcher(
+      isFulfilled(authActions.getAccessToken),
+      (state, action) => {
+        state.token = action.payload?.access_token
+        Cookie.set('TOKEN', state.token, {
+          secure: true,
+          expires: 1,
+        })
+        state.loadingGetAccessToken = false
+      },
+    )
+    builder.addMatcher(isRejected(authActions.getAccessToken), (state) => {
+      state.loadingGetAccessToken = false
+    })
   },
 })
 
