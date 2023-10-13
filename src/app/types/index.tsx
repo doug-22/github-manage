a839ac10fa4438e7061d51b5b75d4675afbc255a
Douglas <douglas.aguiar410@gmail.com>
2023-10-13 16:55:27 -0300

    feat/created new constants and types

--- src/app/types/index.tsx
+++ src/app/types/index.tsx
@@ -1,15 +1,18 @@
+import { ReactNode } from 'react'
+
 export interface AuthState {
   token: string
   loadingGetAccessToken: boolean
 }
 
 export interface ButtonProps {
-  label: string
-  width: string
+  label?: string
+  width?: string
   height?: string
   onClick?: () => void
   disabled?: boolean
-  $background?: 'primary' | 'secondary'
+  $background?: 'primary' | 'secondary' | 'transparent' | 'outline'
+  icon?: ReactNode
 }
 
 export interface InputProps {
@@ -23,9 +26,35 @@ export interface InputProps {
   id: string
   required?: boolean
   errorMessage?: string | boolean
+  $typeInput?: 'login' | 'home'
 }
 
 export interface ActionPayload {
   payload: any
   type: string
+}
+
+export interface FilterAndOrderProps {
+  value: string
+  label: string
+}
+
+export interface InputSearchProps {
+  width: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onClick: () => void
+}
+
+export interface ViewDashboardOptionProps {
+  icon: ReactNode
+  value: string
+  label: string
+}
+
+export interface FiltersProps {
+  filterAndOrder: FilterAndOrderProps | null
+  search: string | null
+  viewFavorites: boolean
+  darkMode: boolean
+  dashboardMode: 'cards' | 'list'
 }
---
+++ src/app/utils/constants.tsx
@@ -0,0 +1,25 @@
+import { FilterAndOrderProps, ViewDashboardOptionProps } from '../types'
+import { PiListFill } from 'react-icons/pi'
+import { MdDashboard } from 'react-icons/md'
+import theme from '../styles/theme'
+
+export const optionsFilterAndOrder: Array<FilterAndOrderProps> = [
+  { value: 'stars', label: 'Stars' },
+  { value: 'forks', label: 'Forks' },
+  { value: 'open issues', label: 'Open Issues' },
+  { value: 'age', label: 'Age' },
+  { value: 'last commit', label: 'Last Commit' },
+]
+
+export const viewDashboardOptions: Array<ViewDashboardOptionProps> = [
+  {
+    icon: <MdDashboard size={30} color={theme.colors.grayFont} />,
+    label: 'Cards',
+    value: 'cards',
+  },
+  {
+    icon: <PiListFill size={30} color={theme.colors.grayFont} />,
+    label: 'List',
+    value: 'list',
+  },
+]
